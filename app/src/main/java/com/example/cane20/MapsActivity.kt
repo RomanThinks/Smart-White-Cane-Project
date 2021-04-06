05dfb85f9432a80d7ad5e624e35d32a1c328678b
RomanThinks <61527005+RomanThinks@users.noreply.github.com>
2021-04-06 11:48:13 -0700

    with previous map data

--- app/src/main/java/com/example/cane20/MapsActivity.kt
+++ app/src/main/java/com/example/cane20/MapsActivity.kt
@@ -1,19 +1,19 @@
 package com.example.cane20
 
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_maps.*
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
